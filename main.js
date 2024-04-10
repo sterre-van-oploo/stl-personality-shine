e61fefb1bfa6c1911c1623258c41bd8b42e56100
sterre-van-oploo <120740209+sterre-van-oploo@users.noreply.github.com>
2024-04-11 00:57:56 +0200

    done

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 // (**)
-// document.addEventListener("DOMContentLoaded", function() {
-//     setTimeout(function() {
-//         var loadingScreen = document.getElementById("loading-screen");
-//         var mainContent = document.getElementById("main-content");
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        var loadingScreen = document.getElementById("loading-screen");
+        var mainContent = document.getElementById("main-content");
 
-//         // Hide the loading screen
-//         loadingScreen.style.opacity = 0;
+        // Hide the loading screen
+        loadingScreen.style.opacity = 0;
 
-//         // After a short delay, hide the loading screen completely and show the main content
-//         setTimeout(function() {
-//             loadingScreen.style.display = "none";
-//             mainContent.style.display = "block";
-//         }, 200); // Adjust the delay based on your needs
-//     }, 5000); // Adjust the timeout value based on the duration of your loading video or GIF
-// });
+        // After a short delay, hide the loading screen completely and show the main content
+        setTimeout(function() {
+            loadingScreen.style.display = "none";
+            mainContent.style.display = "block";
+        }, 200); // Adjust the delay based on your needs
+    }, 5000); // Adjust the timeout value based on the duration of your loading video or GIF
+});
 
 // // Wait for the DOM content to be fully loaded
 // document.addEventListener('DOMContentLoaded', function() {
