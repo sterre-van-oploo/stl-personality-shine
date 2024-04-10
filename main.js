9c4d21ac440b5e69707dff22e264a410f3b4189e
sterre-van-oploo <120740209+sterre-van-oploo@users.noreply.github.com>
2024-04-10 23:47:52 +0200

    finished chapters and animations

--- main.js
+++ main.js
@@ -14,3 +14,24 @@
 //         }, 200); // Adjust the delay based on your needs
 //     }, 5000); // Adjust the timeout value based on the duration of your loading video or GIF
 // });
+
+// // Wait for the DOM content to be fully loaded
+// document.addEventListener('DOMContentLoaded', function() {
+//     // Get the .bottom-parallelogram element
+//     const bottomParallelogram = document.querySelector('.bottom-parallelogram');
+
+//     // Get the .ab-cafe-text element
+//     const abCafeText = document.querySelector('.ab-cafe-text');
+
+//     // Add event listener to toggle visibility of text on mouseover
+//     bottomParallelogram.addEventListener('mouseover', function() {
+//         abCafeText.style.visibility = 'visible';
+//     });
+
+//     // Add event listener to hide text on mouseout
+//     bottomParallelogram.addEventListener('mouseout', function() {
+//         abCafeText.style.visibility = 'hidden';
+//     });
+// });
+
+
